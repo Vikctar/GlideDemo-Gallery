11e27af541474296bc635a77dbf0a86e6b7c86aa
vikctar <vikc.steve@gmail.com>
2016-07-01 04:15:03 +0300

    App is running, but there is a window leak. TF is that??

--- app/src/main/java/com/vikcandroid/glidedemogallery/activity/MainActivity.java
+++ app/src/main/java/com/vikcandroid/glidedemogallery/activity/MainActivity.java
@@ -1,15 +1,102 @@
 package com.vikcandroid.glidedemogallery.activity;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.vikcandroid.glidedemogallery.R;
+import com.vikcandroid.glidedemogallery.adapter.GalleryAdapter;
+import com.vikcandroid.glidedemogallery.app.AppController;
+import com.vikcandroid.glidedemogallery.model.Image;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = MainActivity.class.getSimpleName();
+    private static final String endpoint = "http://api.androidhive.info/json/glide.json";
+    private ArrayList<Image> images;
+    private ProgressDialog pDialog;
+    private GalleryAdapter mAdapter;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        pDialog = new ProgressDialog(this);
+        images = new ArrayList<>();
+        mAdapter = new GalleryAdapter(images, getApplicationContext());
+
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getApplicationContext(), 2);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        // I forgot to call this method
+        // staring at a blank screen and wondering what you did wrong is not pleasing
+        fetchImages();
+    }
+
+    private void fetchImages() {
+        pDialog.setMessage("Downloading...");
+        pDialog.show();
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(endpoint,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+                        pDialog.hide();
+
+                        images.clear();
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                JSONObject jsonObject = response.getJSONObject(i);
+                                Image image = new Image();
+                                image.setName(jsonObject.getString("name"));
+
+                                JSONObject url = jsonObject.getJSONObject("url");
+                                image.setSmall(url.getString("small"));
+                                image.setMedium(url.getString("medium"));
+                                image.setLarge(url.getString("large"));
+                                image.setTimestamp(jsonObject.getString("timestamp"));
+
+                                images.add(image);
+                            } catch (JSONException e) {
+                                Log.e(TAG, "Json parsing error" + e.getMessage());
+                            }
+                        }
+
+                        mAdapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "error" + error.getMessage());
+                pDialog.hide();
+            }
+        });
+
+        // adding request to request queue
+        AppController.getInstance().addToRequestQueue(jsonArrayRequest);
     }
 }
